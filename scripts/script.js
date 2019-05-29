ff45f35e8bb3b5e5fab1539ccfd4bf6fec59e1a6
Citizen-Z <citi@zen.com>
2019-05-29 08:28:09 +0300

    initial upload

---
+++ scripts/script.js
@@ -0,0 +1,251 @@
+$(document).ready(function(){
+	  $('[data-toggle="tooltip"]').tooltip();
+
+
+});
+
+$(document).ready(function() {
+  $("img").unveil();
+});
+/*jslint browser: true */
+/*global XDomainRequest, MutationObserver, window */
+/*svguse.js: */
+(function () {
+    'use strict';
+    if (window && window.addEventListener) {
+        var cache = Object.create(null); // holds xhr objects to prevent multiple requests
+        var checkUseElems,
+            tid; // timeout id
+        var debouncedCheck = function () {
+            clearTimeout(tid);
+            tid = setTimeout(checkUseElems, 100);
+        };
+        var unobserveChanges = function () {
+            return;
+        };
+        var observeChanges = function () {
+            var observer;
+            if (window.MutationObserver) {
+                observer = new MutationObserver(debouncedCheck);
+                observer.observe(document.body, {
+                    childList: true,
+                    subtree: true,
+                    attributes: true,
+                    attributeFilter: ['xlink:href']
+                });
+                unobserveChanges = function () {
+                    try {
+                        observer.disconnect();
+                    } catch (ignore) {}
+                };
+            } else {
+                document.body.addEventListener('DOMSubtreeModified', debouncedCheck, false);
+                unobserveChanges = function () {
+                    document.body.removeEventListener('DOMSubtreeModified', debouncedCheck, false);
+                };
+            }
+        };
+        checkUseElems = function () {
+            var base,
+                bcr,
+                fallback = '', // optional fallback URL in case no base path to SVG file was given and no symbol definition was found.
+                hash,
+                i,
+                Request,
+                inProgressCount = 0,
+                url,
+                uses,
+                xhr;
+            if (window.XMLHttpRequest) {
+                Request = new XMLHttpRequest();
+                if (Request.withCredentials !== undefined) {
+                    Request = XMLHttpRequest;
+                } else {
+                    Request = XDomainRequest || undefined;
+                }
+            }
+            if (Request === undefined) {
+                return;
+            }
+            function observeIfDone() {
+                // If done with making changes, start watching for chagnes in DOM again
+                inProgressCount -= 1;
+                if (inProgressCount === 0) { // if all xhrs were resolved
+                    observeChanges(); // watch for changes to DOM
+                }
+            }
+            function onload(xhr) {
+                return function () {
+                    var body = document.body,
+                        x = document.createElement('x');
+                    xhr.onload = null;
+                    x.innerHTML = xhr.responseText;
+                    body.insertBefore(x.firstChild, body.firstChild);
+                    observeIfDone();
+                };
+            }
+            function onErrorTimeout(xhr) {
+                return function () {
+                    xhr.onerror = null;
+                    xhr.ontimeout = null;
+                    observeIfDone();
+                };
+            }
+            unobserveChanges(); // stop watching for changes to DOM
+            // find all use elements
+            uses = document.getElementsByTagName('use');
+            for (i = 0; i < uses.length; i += 1) {
+                try {
+                    bcr = uses[i].getBoundingClientRect();
+                } catch (e) {
+                    // failed to get bounding rectangle of the use element
+                    bcr = false;
+                }
+                url = uses[i].getAttribute('xlink:href').split('#');
+                base = url[0];
+                hash = url[1];
+                if (bcr && bcr.width === 0 && bcr.height === 0) {
+                    // the use element is empty
+                    // if there is a reference to an external SVG, try to fetch it
+                    // use the optional fallback URL if there is no reference to an external SVG
+                    if (fallback && !base.length && hash && !document.getElementById(hash)) {
+                        base = fallback;
+                    }
+                    if (base.length) {
+                        xhr = cache[base];
+                        if (xhr !== true) {
+                            uses[i].setAttribute('xlink:href', '#' + hash);
+                        }
+                        if (xhr === undefined) {
+                            xhr = new Request();
+                            cache[base] = xhr;
+                            xhr.onload = onload(xhr);
+                            xhr.onerror = onErrorTimeout(xhr);
+                            xhr.ontimeout = onErrorTimeout(xhr);
+                            xhr.open('GET', base);
+                            xhr.send();
+                            inProgressCount += 1;
+                        }
+                    }
+                } else {
+                    // remember this URL if the use element was not empty and no request was sent
+                    if (cache[base] === undefined) {
+                        cache[base] = true;
+                    }
+                }
+            }
+            uses = '';
+            inProgressCount += 1;
+            observeIfDone();
+        };
+        // The load event fires when all resources have finished loading, which allows detecting whether SVG use elements are empty.
+        window.addEventListener('load', function winLoad() {
+            window.removeEventListener('load', winLoad, false); // to prevent memory leaks
+            checkUseElems();
+        }, false);
+    }
+}());
+
+/**
+ * jQuery Unveil
+ * A very lightweight jQuery plugin to lazy load images
+ * http://luis-almeida.github.com/unveil
+ *
+ * Licensed under the MIT license.
+ * Copyright 2013 Luís Almeida
+ * https://github.com/luis-almeida
+ */
+
+;(function($) {
+
+  $.fn.unveil = function(threshold, callback) {
+
+    var $w = $(window),
+        th = threshold || 0,
+        retina = window.devicePixelRatio > 1,
+        attrib = retina? "data-src-retina" : "data-src",
+        images = this,
+        loaded;
+
+    this.one("unveil", function() {
+      var source = this.getAttribute(attrib);
+      source = source || this.getAttribute("data-src");
+      if (source) {
+        this.setAttribute("src", source);
+        if (typeof callback === "function") callback.call(this);
+      }
+    });
+
+    function unveil() {
+      var inview = images.filter(function() {
+        var $e = $(this);
+        if ($e.is(":hidden")) return;
+
+        var wt = $w.scrollTop(),
+            wb = wt + $w.height(),
+            et = $e.offset().top,
+            eb = et + $e.height();
+
+        return eb >= wt - th && et <= wb + th;
+      });
+
+      loaded = inview.trigger("unveil");
+      images = images.not(loaded);
+    }
+
+    $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+    unveil();
+
+    return this;
+
+  };
+
+})(window.jQuery || window.Zepto);
+
+// cookies handling
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1);
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+}
+
+function deleteCookie(cname) {
+  var d = new Date();
+  d.setTime(d.getTime() - 1);
+  var expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=; " + expires;
+}
+
+//Events for ga analytics
+function trackEvent(  EventCategory, // Abroad_provider,Abroad_search,Local_provider
+											EventAction, // domain name, like 	www.radio-rafi.co.il, www.amazon.com
+											EventLabel, // the referral link itself (canonical form)
+											EventValue //  the value of the product (1 for search)
+										)
+	{
+		var send_obj = {
+		  hitType: 'event',
+		  eventCategory: EventCategory.toString(),
+		  eventAction: EventAction.toString(),
+		  //eventLabel: ,
+			//eventValue: EventValue
+		};
+
+		if (EventLabel) {send_obj.eventLabel = EventLabel;}
+		if (EventValue) {send_obj.eventValue = EventValue;}
+
+		return ga('send', send_obj );
+}
